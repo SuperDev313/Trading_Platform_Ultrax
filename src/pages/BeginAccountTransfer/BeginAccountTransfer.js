caeb4155ecd520df2dee1181a8b576307a512a9a
SuperDev313 <pablo313boss@gmail.com>
2024-04-10 17:52:22 -0700

    Trans

--- src/pages/BeginAccountTransfer/BeginAccountTransfer.js
+++ src/pages/BeginAccountTransfer/BeginAccountTransfer.js
@@ -1,16 +1,26 @@
 import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import useSWR from 'swr'
+import { Trans, t } from '@lingui/macro'
+import Modal from 'components/Modal/Modal'
 
 export default function BeginAccountTransfer(props) {
 
-    const [isTransferSubmittedModalVisible, setTransferSubmittedModalVisible] = useState(false)
-    
+    const [isTransferSubmittedModalVisible, setIsTransferSubmittedModalVisible] = useState(false)
+
     return (
         <div className ="BeginAccountTransfer Page page-layout">
             <Modal 
                 isVisible={isTransferSubmittedModalVisible}
-                setIsVisivle = {setTransferSubmittedModalVisible}
+                setIsVisivle={setIsTransferSubmittedModalVisible}
                 label={t`Transfer Submitted`}
             >
+                <Trans>Your transfer has been initiated.</Trans>
+                <br />
+                <br />
+                <Link className="App-cta" to={completeTransferLink}>
+                    <Trans>Continue</Trans>
+                </Link>
             </Modal>
         </div>
     )
