83dff1d85132c4c1e28768756ea8328e7e59af00
SuperDev313 <pablo313boss@gmail.com>
2024-04-11 18:48:17 -0700

    Contract Addresss and Fetch Data

--- src/pages/BeginAccountTransfer/BeginAccountTransfer.js
+++ src/pages/BeginAccountTransfer/BeginAccountTransfer.js
@@ -3,11 +3,47 @@ import {Link} from 'react-router-dom'
 import useSWR from 'swr'
 import { Trans, t } from '@lingui/macro'
 import Modal from 'components/Modal/Modal'
+import { useChainId } from 'lib/chains'
+import { getContract } from 'config/contracts'
+import { contractFetcher } from 'lib/contracts'
 
 export default function BeginAccountTransfer(props) {
+    const { setPendingTxns } = props;
+    const { active, library, account } = useWeb3React();
+    const { chainId } = useChainId();
+  
+    const [receiver, setReceiver] = useState("");
+    const [isTransferring, setIsTransferring] = useState(false);
+    const [isApproving, setIsApproving] = useState(false);
 
     const [isTransferSubmittedModalVisible, setIsTransferSubmittedModalVisible] = useState(false)
 
+    if (ethers.utils.isAddress(receiver)) {
+        parseReceiver = receiver;
+    }
+
+    const utxAddress = getContract(chainId, "UTX")
+    const utxVesterAddress = getContract(chainId, "UtxVester")
+    const ulpVesterAddress = getContract(chainId, "UlpVester")
+    const rewardRouterAddress = getContract(chainId, "RewardRouter")
+    let parseReceiver = ethers.contants.AddressZero;
+
+    const { data: utxVesterBalance } = useSWR(active && [ active, chainId, utxVesterAddress, "balanceOf", account], {
+        fetcher: contractFetcher(library, Token),
+    })
+
+    const { data: ulpVesterBalance } = useSWR(active && [active, chainId, ulpVesterAddress, "balanceOf", account], {
+        fetcher: contractFetcher(library, Token),
+      });
+    
+      const stakedUtxTrackerAddress = getContract(chainId, "StakedUtxTracker");
+      const { data: cumulativeUtxRewards } = useSWR(
+        [active, chainId, stakedUtxTrackerAddress, "cumulativeRewards", parsedReceiver],
+        {
+          fetcher: contractFetcher(library, RewardTracker),
+        }
+      );
+      
     return (
         <div className ="BeginAccountTransfer Page page-layout">
             <Modal 
