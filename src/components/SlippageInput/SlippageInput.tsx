77546505be0efa2dd3e2602dba956a84bb277f87
SuperDev313 <pablo313boss@gmail.com>
2024-04-16 17:21:57 -0700

    constant set

--- src/components/SlippageInput/SlippageInput.tsx
+++ src/components/SlippageInput/SlippageInput.tsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 import cx from "classnames";
 import { formatAmount } from "lib/numbers";
 
+const MAX_SLIPPAGE = 99 * 100;
+const HIGH_SLIPPAGE = 2 * 100;
+const SLIPPAGE_SUGGESTION_LISTS = [0,3, 0.5, 1, 1.5]
+const validDecimalRegex = /^\d+(\.\d{0,2})?$/; // 0.00 ~ 99.99
+
 export default function SlippageInput({ setAllowedSlippage, defaultSlippage }: Props) {
   return (
     <div className="Slippage-input-wrapper">
