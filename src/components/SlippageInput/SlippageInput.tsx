d93125de705ea6cd721d061c412ba443d7176ea1
SuperDev313 <pablo313boss@gmail.com>
2024-04-16 17:24:06 -0700

    function getSlippageText and Props

--- src/components/SlippageInput/SlippageInput.tsx
+++ src/components/SlippageInput/SlippageInput.tsx
@@ -4,8 +4,17 @@ import { formatAmount } from "lib/numbers";
 
 const MAX_SLIPPAGE = 99 * 100;
 const HIGH_SLIPPAGE = 2 * 100;
-const SLIPPAGE_SUGGESTION_LISTS = [0,3, 0.5, 1, 1.5]
+const SLIPPAGE_SUGGESTION_LISTS = [0, 3, 0.5, 1, 1.5];
 const validDecimalRegex = /^\d+(\.\d{0,2})?$/; // 0.00 ~ 99.99
+
+function getSlippageText(value: number) {
+  return formatAmount(value, 2, 2).replace(/0+$/, "");
+}
+
+type Props = {
+  setAllowedSlippage: (value: number) => void;
+  defaultSlippage: number;
+};
 
 export default function SlippageInput({ setAllowedSlippage, defaultSlippage }: Props) {
   return (
