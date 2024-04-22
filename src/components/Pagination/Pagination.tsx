e890c8bedaef01efe939cec905317d597f826939
SuperDev313 <pablo313boss@gmail.com>
2024-04-22 07:57:49 -0700

    Pagination page

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,8 +1,28 @@
+import Button from "components/Button/Button";
+
 type Props = {
   page: number;
   pageCount: number;
   onPageChange: any;
 };
+
 export default function Pagination({ page, pageCount, onPageChange }: Props) {
-  return <div></div>;
+  return (
+    <div className="pagination">
+      <div className="pagination-buttons">
+        <Button variant="secondary" onClick={() => onPageChange(1)} disabled={page <= 1}>
+          {"|<"}
+        </Button>
+        <Button variant="secondary" onClick={() => onPageChange(page - 1)} disabled={page <= 1}>
+          {"<"}
+        </Button>
+        <Button variant="secondary" onClick={() => onPageChange(page + 1)} disabled={page >= pageCount}>
+          {">"}
+        </Button>
+        <Button variant="secondary" onClick={() => onPageChange(pageCount)} disabled={page >= pageCount}>
+          {">|"}
+        </Button>
+      </div>
+    </div>
+  );
 }
