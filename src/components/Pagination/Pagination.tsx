5ad65e0013276bc83987cc91fb5ed59f398ee19f
SuperDev313 <pablo313boss@gmail.com>
2024-04-21 18:37:15 -0700

    Pagination Component File created

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+type Props = {
+  page: number;
+  pageCount: number;
+  onPageChange: any;
+};
+export default function Pagination({ page, pageCount, onPageChange }: Props) {
+  return <div></div>;
+}
