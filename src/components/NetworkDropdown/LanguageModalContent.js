f1ec45e74f4afafb4456d7c9ce44c8d0ac03bc35
SuperDev313 <pablo313boss@gmail.com>
2024-05-01 20:28:40 -0700

    Language Modal Page

--- src/components/NetworkDropdown/LanguageModalContent.js
+++ src/components/NetworkDropdown/LanguageModalContent.js
@@ -1,3 +1,21 @@
+import { LANGUAGE_LOCALSTORAGE_KEY } from "config/localStorage";
+import { dynamicActivate } from "lib/i18n";
+
 export default function LanguageModalContent({ currentLanguage }) {
-  return <div></div>;
+  return (
+    <div
+      key={item}
+      className={CSSSkewX("network-dropdown-menu-item menu-item language-modal-item", {
+        active: currentLanguage.current === item,
+      })}
+      onClick={() => {
+        if (!isTestLanguage(item)) {
+          localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, item);
+        }
+        dynamicActivate(item);
+      }}
+    >
+      <div className="menu-item-group"></div>
+    </div>
+  );
 }
