f10bf2a54a1d2ac3284ca9db379f90e827d0e6ca
SuperDev313 <pablo313boss@gmail.com>
2024-05-02 10:34:06 -0700

    LanguageModalContact Component completed

--- src/components/NetworkDropdown/LanguageModalContent.tsx
+++ src/components/NetworkDropdown/LanguageModalContent.tsx
@@ -9,29 +9,41 @@ type Props = {
     current: string | undefined;
   };
 };
+
 export default function LanguageModalContent({ currentLanguage }: Props) {
   return (
-    <div
-      key={item}
-      className={cx("network-dropdown-menu-item  menu-item language-modal-item", {
-        active: currentLanguage.current === item,
+    <>
+      {Object.keys(locales).map((item) => {
+        const image = importImage(`flag_${item}.svg`);
+        return (
+          <div
+            key={item}
+            className={cx("network-dropdown-menu-item  menu-item language-modal-item", {
+              active: currentLanguage.current === item,
+            })}
+            onClick={() => {
+              if (!isTestLanguage(item)) {
+                localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, item);
+              }
+              dynamicActivate(item);
+            }}
+          >
+            <div className="menu-item-group">
+              <div className="menu-item-icon">
+                {isTestLanguage(item) ? (
+                  "🫐"
+                ) : (
+                  <img className="network-dropdown-icon" src={image} alt={locales[item]} />
+                )}
+              </div>
+              <span className="network-dropdown-item-label menu-item-label">{locales[item]}</span>
+            </div>
+            <div className="network-dropdown-menu-item-img">
+              {currentLanguage.current === item && <img src={checkedIcon} alt={locales[item]} />}
+            </div>
+          </div>
+        );
       })}
-      onClick={() => {
-        if (!isTestLanguage(item)) {
-          localStorage.setItem(LANGUAGE_LOCALSTORAGE_KEY, item);
-        }
-        dynamicActivate(item);
-      }}
-    >
-      <div className="menu-item-group">
-        <div className="menu-item-icon">
-          {isTestLanguage(item) ? "🫐" : <img className="network-dropdown-icon" src={image} alt={locales[item]} />}
-        </div>
-        <span className="network-dropdown-item-label menu-item-label">{locales[item]}</span>
-      </div>
-      <div className="network-dropdown-menu-item-img">
-        {currentLanguage.current === item && <img src={checkedIcon} alt={locales[item]} />}
-      </div>
-    </div>
+    </>
   );
 }
