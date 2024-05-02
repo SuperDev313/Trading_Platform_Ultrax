dfeaf322a752a9ea019855f7e9c92a337a268eba
SuperDev313 <pablo313boss@gmail.com>
2024-05-02 10:56:50 -0700

    Constant sets

--- src/components/NetworkDropdown/NetworkDropdown.tsx
+++ src/components/NetworkDropdown/NetworkDropdown.tsx
@@ -1,7 +1,9 @@
+import { LANGUAGE_LOCALSTORAGE_KEY } from "config/localStorage";
+import { defaultLocale } from "lib/i18n";
 import React, { useRef, useState } from "react";
 
 export default function NetworkDropdown(props) {
-    return (
-        <></>
-    )
+  const currentLanguage = useRef(localStorage.getItem(LANGUAGE_LOCALSTORAGE_KEY) || defaultLocale);
+  const [activeModal, setActiveModal] = useState<string | null>(null);
+  return <></>;
 }
