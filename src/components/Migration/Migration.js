28a9a87aa4099eafe6555c85add5b79329c35965
SuperDev313 <pablo313boss@gmail.com>
2024-05-06 13:44:12 -0700

    Value sets

--- src/components/Migration/Migration.js
+++ src/components/Migration/Migration.js
@@ -1,3 +1,11 @@
 export default function Migration() {
+  const [isMigrationModalVisible, setIsMigrationModalVisible] = useState(false);
+  const [isPendingApproval, setIsPendingApproval] = useState(false);
+  const [migrationIndex, setMigrationIndex] = useState(0);
+  const [migrationValue, setMigrationValue] = useState("");
+
+  const { connector, activate, active, account, library } = useWeb3React();
+  const [activatingConnector, setActivatingConnector] = useState();
+
   return <></>;
 }
