745ff0ae05cd4e20f506f5395e7386707e5e9dff
SuperDev313 <pablo313boss@gmail.com>
2024-05-06 22:46:42 -0500

    Page title section

--- src/pages/TermsAndConditions/TermsAndConditions.js
+++ src/pages/TermsAndConditions/TermsAndConditions.js
@@ -6,5 +6,17 @@ import { getPageTitle } from "lib/legacy";
 import "./TermsAndConditions.css";
 
 export default function TermsAndConditions() {
-  return <SEO title={getPageTitle(t`Terms and Conditions`)}></SEO>;
+  return (
+    <SEO title={getPageTitle(t`Terms and Conditions`)}>
+      <div className="default-container Page page-layout">
+        <div>
+          <div className="Page-title-section center">
+            <div className="Page-title">UTX</div>
+            <div className="Page-subtitle">Terms and Conditions</div>
+            <div className="Page-description">Last modified: August 1st, 2022</div>
+          </div>
+        </div>
+      </div>
+    </SEO>
+  );
 }
