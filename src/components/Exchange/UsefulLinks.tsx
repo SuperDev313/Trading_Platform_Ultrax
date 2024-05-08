20cc22a9cee24565c9045c7d5ae4b2f3a3a4679f
SuperDev313 <pablo313boss@gmail.com>
2024-05-07 21:21:50 -0500

    UsefulLinks component done

--- src/components/Exchange/UsefulLinks.tsx
+++ src/components/Exchange/UsefulLinks.tsx
@@ -1,12 +1,15 @@
 import { Trans } from "@lingui/macro";
+import ExternalLink from "components/ExternalLink/ExternalLink";
+import { getLeaderboardLink } from "config/links";
+import cx from "classnames";
+import { useChainId } from "lib/chains";
 
 export default function UsefulLinks({ className }) {
   const { chainId } = useChainId();
   const leaderBoardLink = getLeaderboardLink(chainId);
   const classNames = cx("Exchange-swap-market-box App-box App-box-border", className);
-  
   return (
-    <div className={className}>
+    <div className={classNames}>
       <div className="Exchange-swap-market-box-title">
         <Trans>Useful Links</Trans>
       </div>
@@ -21,6 +24,13 @@ export default function UsefulLinks({ className }) {
         <div className="Exchange-info-label-button">
           <ExternalLink href={""} className="text-accent fz-sm fw-400">
             <Trans>Leaderboard</Trans>
+          </ExternalLink>
+        </div>
+      </div>
+      <div className="Exchange-info-row">
+        <div className="Exchange-info-label-button">
+          <ExternalLink href="" className="text-accent fz-sm fw-400">
+            <Trans>Speed up page loading</Trans>
           </ExternalLink>
         </div>
       </div>
