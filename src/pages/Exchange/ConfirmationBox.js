f64756d8da9ef7913bd7a5749b00223290804a92
SuperDev313 <pablo313boss@gmail.com>
2024-05-08 22:44:36 -0500

    Function GetPrimaryText

--- src/pages/Exchange/ConfirmationBox.js
+++ src/pages/Exchange/ConfirmationBox.js
@@ -16,7 +16,98 @@ import {
   DECREASE,
 } from "lib/legacy";
 
+import Modal from "../Modal/Modal";
+import Button from "components/Button/Button";
+
 export default function ConfirmationBox(props) {
+  const {
+    fromToken,
+    fromTokenInfo,
+    toToken,
+    toTokenInfo,
+    isSwap,
+    isLong,
+    isMarketOrder,
+    orderOption,
+    isShort,
+    toAmount,
+    fromAmount,
+    isHigherSlippageAllowed,
+    onConfirmationClick,
+    setIsConfirming,
+    hasExistingPosition,
+    leverage,
+    existingPosition,
+    existingLiquidationPrice,
+    displayLiquidationPrice,
+    shortCollateralToken,
+    isPendingConfirmation,
+    triggerPriceUsd,
+    triggerRatio,
+    feesUsd,
+    isSubmitting,
+    fromUsdMin,
+    toUsdMax,
+    nextAveragePrice,
+    collateralTokenAddress,
+    feeBps,
+    chainId,
+    orders,
+    library,
+    setPendingTxns,
+    pendingTxns,
+    minExecutionFee,
+    minExecutionFeeUSD,
+    minExecutionFeeErrorMessage,
+    entryMarkPrice,
+    positionFee,
+    swapFees,
+    infoTokens,
+    fundingRate,
+    borrowFeeText,
+  } = props;
+
+  const getPrimaryText = () => {
+    if (decreaseOrdersThatWillBeExecuted.length > 0 && !isTriggerWarningAccepted) {
+      return t`Accept confirmation of trigger orders`;
+    }
+
+    if (!isPendingConfirmation) {
+      const error = getError();
+      if (error) {
+        return error;
+      }
+
+      if (isSwap) {
+        return title;
+      }
+      const action = isLong ? t`Long` : t`Short`;
+
+      if (
+        isMarketOrder &&
+        MIN_PROFIT_TIME > 0 &&
+        hasExistingPosition &&
+        existingPosition.delta.eq(0) &&
+        existingPosition.pendingDelta.gt(0)
+      ) {
+        return isLong ? t`Forfeit profit and ${action}` : t`Forfeit profit and Short`;
+      }
+
+      return isMarketOrder && MIN_PROFIT_TIME > 0 ? t`Accept minimum and ${action}` : action;
+    }
+
+    if (!isMarketOrder) {
+      return t`Creating Order...`;
+    }
+    if (isSwap) {
+      return t`Swapping...`;
+    }
+    if (isLong) {
+      return t`Longing...`;
+    }
+    return t`Shorting...`;
+  };
+
   return (
     <div className="Confirmation-box">
       <Modal isVisible={true} setIsVisible={() => setIsConfirming(false)} label={title}>
