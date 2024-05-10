c9e0d8712304575de7cb7a1d64e223216b0e94ce
SuperDev313 <pablo313boss@gmail.com>
2024-05-09 22:13:05 -0500

    Exchange List Table in OrdersList

---
+++ src/pages/Exchange/OrdersList.js
@@ -0,0 +1,46 @@
+import React, { useState, useCallback } from "react";
+import { t, Trans } from "@lingui/macro";
+import cancelX from "../../img/cancelX.svg";
+import {
+  SWAP,
+  INCREASE,
+  DECREASE,
+  USD_DECIMALS,
+  getOrderError,
+  getExchangeRateDisplay,
+  getExchangeRate,
+  getPositionForOrder,
+} from "lib/legacy";
+import { handleCancelOrder } from "domain/legacy";
+import { getContract } from "config/contracts";
+
+export default function OrdersList(props) {
+  const {
+    account,
+    library,
+    setPendingTxns,
+    pendingTxns,
+    infoTokens,
+    positionsMap,
+    totalTokenWeights,
+    usdgSupply,
+    orders,
+    hideActions,
+    chainId,
+    savedShouldDisableValidationForTesting,
+    cancelOrderIdList,
+    setCancelOrderIdList,
+  } = props;
+
+  return (
+    <React.Fragment>
+      <table className="Exchange-list Orders large App-box">
+        <tbody>
+          {!!account && !!orders && !!orders.length && renderHead()}
+          {renderEmptyRow()}
+          <div className="Exchange-list-container">{renderLargeList()}</div>
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
+}
