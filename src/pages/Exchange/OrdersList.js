5430b265bce662947cf35300e337d836f50fe8d1
SuperDev313 <pablo313boss@gmail.com>
2024-05-10 13:55:54 -0500

    RenderSmallList

--- src/pages/Exchange/OrdersList.js
+++ src/pages/Exchange/OrdersList.js
@@ -295,7 +295,179 @@ export default function OrdersList(props) {
     });
   }, [orders, renderActions, infoTokens, positionsMap, hideActions, chainId, account, onCancelClick, onEditClick]);
 
-    return (
+  const renderSmallList = useCallback(() => {
+    return orders.reverse().map((order) => {
+      if (order.type === SWAP) {
+        const nativeTokenAddress = getContract(chainId, "NATIVE_TOKEN");
+        const fromTokenInfo = getTokenInfo(infoTokens, order.path[0], true, nativeTokenAddress.toLowerCase());
+        const toTokenInfo = getTokenInfo(
+          infoTokens,
+          order.path[order.path.length - 1],
+          order.shouldUnwrap,
+          nativeTokenAddress.toLowerCase()
+        );
+        const markExchangeRate = getExchangeRate(fromTokenInfo, toTokenInfo);
+        const collateralUSD = getUsd(order.amountIn, fromTokenInfo.address, true, infoTokens);
+        const titleText = (
+          <>
+            Swap {formatAmount(order.amountIn, fromTokenInfo.decimals, fromTokenInfo.isStable ? 2 : 4, true)}{" "}
+            {fromTokenInfo?.symbol} for{" "}
+            {formatAmount(order.minOut, toTokenInfo.decimals, toTokenInfo.isStable ? 2 : 4, true)} {toTokenInfo?.symbol}
+          </>
+        );
+        return (
+          <div key={`${order.type}-${order.index}`} className="App-card">
+            <div className="App-card-content">
+              <div className="App-card-title-small">{titleText}</div>
+              <div className="App-card-divider"></div>
+              <div className="App-card-row">
+                <div className="label">
+                  <Trans>Price</Trans>
+                </div>
+                <div>
+                  <Tooltip
+                    position="right-bottom"
+                    handle={getExchangeRateDisplay(order.triggerRatio, fromTokenInfo, toTokenInfo)}
+                    renderContent={() => t`
+                    You will receive at least ${formatAmount(
+                      order.minOut,
+                      toTokenInfo.decimals,
+                      toTokenInfo.isStable || toTokenInfo.isUsdg ? 2 : 4,
+                      true
+                    )} ${
+                      toTokenInfo?.symbol
+                    } if this order is executed. The exact execution price may vary depending on fees at the time the order is executed.
+                  `}
+                  />
+                </div>
+              </div>
+              <div className="App-card-row">
+                <div className="label">
+                  <Trans>Mark Price</Trans>
+                </div>
+                <div>{getExchangeRateDisplay(markExchangeRate, fromTokenInfo, toTokenInfo)}</div>
+              </div>
+              <div className="App-card-row">
+                <div className="label">
+                  <Trans>Collateral</Trans>
+                </div>
+                <div>
+                  ${formatAmount(collateralUSD, USD_DECIMALS, 2, true)} (
+                  {formatAmount(order.amountIn, fromTokenInfo.decimals, 4, true)}{" "}
+                  {fromTokenInfo.baseSymbol || fromTokenInfo?.symbol})
+                </div>
+              </div>
+            </div>
+            <div>
+              {!hideActions && (
+                <>
+                  <div className="App-card-divider"></div>
+                  <div className="App-card-options">
+                    <button className="App-button-option App-card-option" onClick={() => onEditClick(order)}>
+                      <Trans>Edit</Trans>
+                    </button>
+                    <button className="App-button-option App-card-option" onClick={() => onCancelClick(order)}>
+                      <Trans>Cancel</Trans>
+                    </button>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+        );
+      }
+
+      const indexToken = getTokenInfo(infoTokens, order.indexToken.toLowerCase());
+      const indexTokenSymbol = indexToken?.isWrapped ? indexToken?.baseSymbol : indexToken?.symbol;
+      // const maximisePrice = (order.type === INCREASE && order.isLong) || (order.type === DECREASE && !order.isLong);
+      // const markPrice = maximisePrice ? indexToken.contractMaxPrice : indexToken.contractMinPrice;
+      // const triggerPricePrefix = order.triggerAboveThreshold ? TRIGGER_PREFIX_ABOVE : TRIGGER_PREFIX_BELOW;
+
+      // const collateralTokenInfo = getTokenInfo(infoTokens, order.purchaseToken);
+      // const collateralUSD = getUsd(order.purchaseTokenAmount, order.purchaseToken, true, infoTokens);
+
+      const error = getOrderError(account, order, positionsMap);
+      const orderTitle = getOrderTitle(order, indexTokenSymbol);
+      const longShortText = order.isLong ? t`Long` : t`Short`;
+      const sizeDeltaText = formatAmount(order.sizeDelta, USD_DECIMALS, 2, true);
+
+      return (
+        <div key={`${order.isLong}-${order.type}-${order.index}`} className="App-card App-card-container">
+          <div className="App-card-content">
+            <div className="App-card-title-small">
+              <div className="App-card-title-small-left">
+                <div style={{ fontSize: "18px", fontWeight: "600" }}>
+                  {error ? (
+                    <Tooltip
+                      className="order-error"
+                      handle={indexTokenSymbol}
+                      position="left-bottom"
+                      handleClassName="plain"
+                      renderContent={() => <span className="negative">{error}</span>}
+                    />
+                  ) : (
+                    indexTokenSymbol
+                  )}
+                </div>
+                <div
+                  className="App-card-title-small-long-short"
+                  style={{
+                    background: order.isLong ? "rgba(46, 199, 135, 0.10)" : "rgba(229, 97, 97, 0.10)",
+                    border: order.isLong ? "1px solid #3FB68B" : "1px solid #FF5353",
+                    color: order.isLong ? "#3FB68B" : "#FF5353",
+                  }}
+                >
+                  {longShortText}
+                </div>
+              </div>
+              {!hideActions && (
+                <div className="App-card-title-small-right">
+                  <button className="Exchange-list-action" onClick={() => onEditClick(order)}>
+                    <Trans>Edit</Trans>
+                  </button>
+                  <div className="App-card-title-small-right-divider"></div>
+                  <button className="Exchange-list-action" onClick={() => onCancelClick(order)}>
+                    <img src={cancelX} alt="camcelX" />
+                  </button>
+                </div>
+              )}
+            </div>
+            <div className="App-card-divider"></div>
+            <div className="App-card-row App-card-row-container">
+              <div className="App-card-row-item">
+                <div className="label">
+                  <Trans>Order</Trans>
+                </div>
+                <div>{orderTitle}</div>
+              </div>
+              <div className="App-card-row-item">
+                <div className="label">
+                  <Trans>Type</Trans>
+                </div>
+                <div>{order.type === INCREASE ? t`Limit` : t`Trigger`}</div>
+              </div>
+            </div>
+            <div className="App-card-row App-card-row-container">
+              <div className="App-card-row-item">
+                <div className="label">
+                  <Trans>Order Price</Trans>
+                </div>
+                <div>${formatAmount(order.triggerPrice, USD_DECIMALS, 2, true)}</div>
+              </div>
+              <div className="App-card-row-item">
+                <div className="label">
+                  <Trans>Amount</Trans>
+                </div>
+                <div>${sizeDeltaText}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }, [orders, onEditClick, onCancelClick, infoTokens, positionsMap, hideActions, chainId, account]);
+
+  return (
     <React.Fragment>
       <table className="Exchange-list Orders large App-box">
         <tbody>
