c86648e97dfd2e965ead2c71cad801e844250770
SuperDev313 <pablo313boss@gmail.com>
2024-05-09 22:14:19 -0500

    EditingOrder in OrdersList

--- src/pages/Exchange/OrdersList.js
+++ src/pages/Exchange/OrdersList.js
@@ -41,6 +41,60 @@ export default function OrdersList(props) {
           <div className="Exchange-list-container">{renderLargeList()}</div>
         </tbody>
       </table>
+      {!account ? (
+        <div className="Exchange-list Orders small no-order">
+          <div className="Exchange-list-no-connect">
+            <span className="Exchange-list-no-connect-title">Wallet Required</span>
+            <span className="Exchange-list-no-connect-text">Connect wallet to view your positions</span>
+          </div>
+        </div>
+      ) : orders.length === 0 ? (
+        <div className="Exchange-list Orders small no-order">
+          <div
+            style={{
+              width: "100%",
+            }}
+          >
+            <div
+              className="fz-lg fw-600 text-primary"
+              style={{
+                textAlign: "center",
+                marginTop: "4rem",
+              }}
+            >
+              No Order Found
+            </div>
+            <div
+              className="fz-sm fw-500 text-secondary"
+              style={{
+                textAlign: "center",
+                marginTop: "0.5rem",
+                marginBottom: "4rem",
+              }}
+            >
+              You have no open order
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="Exchange-list Orders small order-list">{renderSmallList()}</div>
+      )}
+      {editingOrder && (
+        <OrderEditor
+          account={account}
+          order={editingOrder}
+          setEditingOrder={setEditingOrder}
+          infoTokens={infoTokens}
+          pendingTxns={pendingTxns}
+          setPendingTxns={setPendingTxns}
+          getPositionForOrder={getPositionForOrder}
+          positionsMap={positionsMap}
+          library={library}
+          totalTokenWeights={totalTokenWeights}
+          usdgSupply={usdgSupply}
+          savedShouldDisableValidationForTesting={savedShouldDisableValidationForTesting}
+        />
+      )}
     </React.Fragment>
   );
 }
