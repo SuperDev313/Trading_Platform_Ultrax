bc16010151723da4023f3cd3cdbe8ed00dc73ba5
SuperDev313 <pablo313boss@gmail.com>
2024-05-09 20:50:40 -0500

    ExchangeWalletToken Component Init

---
+++ src/pages/Exchange/ExchangeWalletToken.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { bigNumberify, expandDecimals, formatAmount } from "lib/numbers";
+
+export default function ExchangeWalletTokens(props) {
+  const { tokens, mintingCap, infoTokens, onSelectToken } = props;
+
+  return <div className="ExchangeWalletTokens App-box"></div>;
+}
