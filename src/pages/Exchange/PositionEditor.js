028cf5d9817d5d8a65eef375fc352759f6b0f66d
SuperDev313 <pablo313boss@gmail.com>
2024-05-10 14:33:45 -0500

    Initial Variable and Function GetError

--- src/pages/Exchange/PositionEditor.js
+++ src/pages/Exchange/PositionEditor.js
@@ -17,6 +17,27 @@ import { getContract } from "config/contracts";
 import Tab from "../Tab/Tab";
 import Modal from "../Modal/Modal";
 
+import PositionRouter from "abis/PositionRouter.json";
+import Token from "abis/Token.json";
+import Tooltip from "../Tooltip/Tooltip";
+
+import { getChainName, IS_NETWORK_DISABLED } from "config/chains";
+import { callContract, contractFetcher } from "lib/contracts";
+import { helperToast } from "lib/helperToast";
+import { getTokenInfo } from "domain/tokens/utils";
+import { approveTokens, shouldRaiseGasError } from "domain/tokens";
+import { usePrevious } from "lib/usePrevious";
+import { bigNumberify, expandDecimals, formatAmount, formatAmountFree, parseValue } from "lib/numbers";
+import { ErrorCode, ErrorDisplayType } from "./constants";
+import Button from "components/Button/Button";
+import FeesTooltip from "./FeesTooltip";
+
+const DEPOSIT = "Deposit";
+const WITHDRAW = "Withdraw";
+const EDIT_OPTIONS = [DEPOSIT, WITHDRAW];
+const MIN_ORDER_USD = expandDecimals(10, USD_DECIMALS);
+const { AddressZero } = ethers.constants;
+
 export default function PositionEditor(props) {
   const {
     pendingPositions,
@@ -45,6 +66,170 @@ export default function PositionEditor(props) {
     minExecutionFeeErrorMessage,
     isContractAccount,
   } = props;
+
+  const nativeTokenAddress = getContract(chainId, "NATIVE_TOKEN");
+  const position = positionsMap && positionKey ? positionsMap[positionKey] : undefined;
+  const [option, setOption] = useState(DEPOSIT);
+  const [fromValue, setFromValue] = useState("");
+  const [isApproving, setIsApproving] = useState(false);
+  const [isSwapping, setIsSwapping] = useState(false);
+  const prevIsVisible = usePrevious(isVisible);
+  const longOrShortText = position?.isLong ? t`Long` : t`Short`;
+
+  const routerAddress = getContract(chainId, "Router");
+  const positionRouterAddress = getContract(chainId, "PositionRouter");
+
+  const { data: tokenAllowance } = useSWR(
+    [active, chainId, collateralTokenAddress, "allowance", account, routerAddress],
+    {
+      fetcher: contractFetcher(library, Token),
+    }
+  );
+
+  const isDeposit = option === DEPOSIT;
+  const isWithdrawal = option === WITHDRAW;
+
+  const needPositionRouterApproval = !positionRouterApproved;
+
+  let collateralToken;
+  let maxAmount;
+  let maxAmountFormatted;
+  let maxAmountFormattedFree;
+  let fromAmount;
+  let needApproval;
+
+  let convertedAmount;
+  let convertedAmountFormatted;
+
+  let nextLeverage;
+  let nextLeverageExcludingPnl;
+  let liquidationPrice;
+  let nextLiquidationPrice;
+  let nextCollateral;
+
+  let title;
+  let collateralDelta;
+  let fundingFee;
+  let depositFeeUSD;
+
+  if (position) {
+    title = t`Edit ${longOrShortText} ${position.indexToken.symbol}`;
+    collateralToken = position.collateralToken;
+    liquidationPrice = getLiquidationPrice(position);
+    fundingFee = getFundingFee(position);
+
+    if (isDeposit) {
+      fromAmount = parseValue(fromValue, collateralToken.decimals);
+      maxAmount = collateralToken ? collateralToken.balance : bigNumberify(0);
+      maxAmountFormatted = formatAmount(maxAmount, collateralToken.decimals, 4, true);
+      maxAmountFormattedFree = formatAmountFree(maxAmount, collateralToken.decimals, 8);
+      if (fromAmount) {
+        convertedAmount = getUsd(fromAmount, position.collateralToken.address, false, infoTokens);
+        convertedAmountFormatted = formatAmount(convertedAmount, USD_DECIMALS, 2);
+      }
+    } else {
+      fromAmount = parseValue(fromValue, USD_DECIMALS);
+
+      maxAmount = position.collateralAfterFee.sub(MIN_ORDER_USD).gt(0)
+        ? position.collateralAfterFee.sub(MIN_ORDER_USD)
+        : bigNumberify(0);
+
+      maxAmountFormatted = formatAmount(maxAmount, USD_DECIMALS, 2, true);
+      maxAmountFormattedFree = formatAmountFree(maxAmount, USD_DECIMALS, 2);
+      if (fromAmount) {
+        convertedAmount = fromAmount.mul(expandDecimals(1, collateralToken.decimals)).div(collateralToken.maxPrice);
+        convertedAmountFormatted = formatAmount(convertedAmount, collateralToken.decimals, 4, true);
+      }
+    }
+    needApproval = isDeposit && tokenAllowance && fromAmount && fromAmount.gt(tokenAllowance);
+
+    if (fromAmount) {
+      collateralDelta = isDeposit ? convertedAmount : fromAmount;
+
+      if (position.isLong && isDeposit) {
+        collateralDelta = collateralDelta.mul(BASIS_POINTS_DIVISOR - DEPOSIT_FEE).div(BASIS_POINTS_DIVISOR);
+        depositFeeUSD = convertedAmount.mul(DEPOSIT_FEE).div(BASIS_POINTS_DIVISOR);
+      }
+
+      nextLeverage = getLeverage({
+        size: position.size,
+        collateral: position.collateral,
+        collateralDelta,
+        increaseCollateral: isDeposit,
+        entryFundingRate: position.entryFundingRate,
+        cumulativeFundingRate: position.cumulativeFundingRate,
+        hasProfit: position.hasProfit,
+        delta: position.delta,
+        includeDelta: savedIsPnlInLeverage,
+      });
+
+      nextLeverageExcludingPnl = getLeverage({
+        size: position.size,
+        collateral: position.collateral,
+        collateralDelta,
+        increaseCollateral: isDeposit,
+        entryFundingRate: position.entryFundingRate,
+        cumulativeFundingRate: position.cumulativeFundingRate,
+        hasProfit: position.hasProfit,
+        delta: position.delta,
+        includeDelta: false,
+      });
+
+      nextLiquidationPrice = getLiquidationPrice({
+        isLong: position.isLong,
+        size: position.size,
+        collateral: position.collateral,
+        averagePrice: position.averagePrice,
+        entryFundingRate: position.entryFundingRate,
+        cumulativeFundingRate: position.cumulativeFundingRate,
+        collateralDelta,
+        increaseCollateral: isDeposit,
+      });
+
+      nextCollateral = isDeposit
+        ? position.collateralAfterFee.add(collateralDelta)
+        : position.collateralAfterFee.sub(collateralDelta);
+    }
+  }
+
+  const getError = () => {
+    if (IS_NETWORK_DISABLED[chainId]) {
+      if (isDeposit) return [t`Deposit disabled, pending ${getChainName(chainId)} upgrade`];
+      return [t`Withdraw disabled, pending ${getChainName(chainId)} upgrade`];
+    }
+
+    if (!fromAmount) {
+      return [t`Enter an amount`];
+    }
+
+    if (fromAmount.lte(0)) {
+      return [t`Amount should be greater than zero`];
+    }
+
+    if (!isDeposit && fromAmount) {
+      if (position.collateralAfterFee.sub(fromAmount).lt(MIN_ORDER_USD)) {
+        return [t`Min residual collateral: 10 USD`];
+      }
+    }
+
+    if (!isDeposit && fromAmount && nextLiquidationPrice) {
+      if (position.isLong && position.markPrice.lt(nextLiquidationPrice)) {
+        return [t`Invalid liq. price`, ErrorDisplayType.Tooltip, ErrorCode.InvalidLiqPrice];
+      }
+      if (!position.isLong && position.markPrice.gt(nextLiquidationPrice)) {
+        return [t`Invalid liq. price`, ErrorDisplayType.Tooltip, ErrorCode.InvalidLiqPrice];
+      }
+    }
+
+    if (nextLeverageExcludingPnl && nextLeverageExcludingPnl.lt(1.1 * BASIS_POINTS_DIVISOR)) {
+      return [t`Min leverage: 1.1x`];
+    }
+
+    if (nextLeverage && nextLeverage.gt(MAX_ALLOWED_LEVERAGE)) {
+      return [t`Max leverage: ${(MAX_ALLOWED_LEVERAGE / BASIS_POINTS_DIVISOR).toFixed(1)}x`];
+    }
+    return [false];
+  };
 
   return (
     <div className="PositionEditor">
