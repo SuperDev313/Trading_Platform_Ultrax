5fbe3e7de9639632c0314effda7456d5100f0e5b
SuperDev313 <pablo313boss@gmail.com>
2024-05-12 07:54:15 -0500

    PositionList Component Init

---
+++ src/pages/Exchange/PositionList.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import cx from "classnames";
+import { Trans, t } from "@lingui/macro";
+
+export default function PositionList(props) {
+  return <div className="PositionsList"></div>;
+}
